77e131b1acf99a321c6ad7175696ae58acb00660
Charlie Kozey <charkoz@mailbox.org>
2023-07-25 11:51:32 -0400

    add chromatic scale keys

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// TODO NEXT: polyphony; maybe by creating a stack of AudioContexts? 
+// TODO NEXT: polyphony; maybe by creating a stack of AudioContexts? (no, hashmap is better)
 // Then startSound() would push a new AudioContext and connect each oscillator to it
 // and stopSound() would remove them all
 
+// TODO: 
+// waveform manipulation (asdr, lfo)
+// effects
+// patches
+// microtonality?
+// user login
+// beginner-friendly illustrations and self-guiding UI
+
 const oscGenerators = document.getElementById("osc-generators")
-const notes = {"a": 220, "s": 247, "d": 277, "f": 294, "g": 330, "h": 370, "j": 415, "k": 440, "l": 494}
+const notes = {"a": 262, "w": 277, "s": 294, "e": 311, "d": 330, "f": 349, "t": 370, "g": 392, "y": 415, "h": 440, "u": 466, "j": 494, "k": 523, "o": 554, "l": 587, "p": 622, ";": 659, "'": 698}
+const audioContexts = []
+const oscillators = []
+const oscNodes = []
 let audioContext
-let oscillators = []
-let oscNodes = []
 let playing = false
 
 function addOscillator(e) {
@@ -22,7 +31,6 @@ function addOscillator(e) {
 function startSound(e) {
     const input = e.key
     audioContext = new AudioContext()
-    console.log(oscillators)
 
     if(Object.keys(notes).includes(input) && !playing) {
         oscillators.forEach(osc => {
@@ -50,7 +58,17 @@ oscGenerators.addEventListener("click", e => addOscillator(e))
 document.addEventListener("keydown", e => startSound(e))
 document.addEventListener("keyup", e => stopSound(e))
 
-// SOMEDAY:
-// user login
-// save patches
-// beginner-friendly illustrations and self-guiding UI
+
+// testing audiocontext stack plan
+
+function pushAudioContext(e) {
+    const keys = ["z", "x", "c", "v", "b", "n", "m"]
+    if (keys.includes(e.key)) {
+        const newAudioContext = new AudioContext()
+        audioContexts.push(newAudioContext)
+
+        console.log(audioContexts)
+    }
+}
+
+document.addEventListener("keypress", e => pushAudioContext(e))
