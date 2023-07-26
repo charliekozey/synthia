97361df29fd3b6f51a78650e1434b9f1fb2b05fd
Charlie Kozey <charkoz@mailbox.org>
2023-07-26 13:17:35 -0400

    create gain slider

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// TODO NEXT: polyphony; maybe by creating a stack of AudioContexts? (no, hashmap is better)
-// Then startSound() would push a new AudioContext and connect each oscillator to it
-// and stopSound() would remove them all
+// TODO NEXT: 
+// gain control for individual oscillators
+// waveform manipulation (asdr, lfo) for individual oscillators
 
 // TODO: 
-// waveform manipulation (asdr, lfo)
+// ~ o s c i l l o s c o p e ~
 // effects
+// lfo
+// EQ/filters
 // patches
 // microtonality?
 // user login
 // beginner-friendly illustrations and self-guiding UI
 // sequencer
+// play more than 6 notes at a time?
+// stereo? spatial??
+// fix browser tab change bug (audio still plays)
 
-const oscGenerators = document.getElementById("osc-generators")
 const keyboard = {
     "a": {freq: 262, down: false}, 
     "w": {freq: 277, down: false}, 
@@ -19,7 +23,7 @@ const keyboard = {
     "e": {freq: 311, down: false}, 
     "d": {freq: 330, down: false}, 
     "f": {freq: 349, down: false}, 
-    "t": {freq: 370, down: false}, 
+    "t": {freq: 370, down: false},
     "g": {freq: 392, down: false}, 
     "y": {freq: 415, down: false}, 
     "h": {freq: 440, down: false}, 
@@ -32,6 +36,7 @@ const keyboard = {
     ";": {freq: 659, down: false}, 
     "'": {freq: 698, down: false},
 }
+const oscGenerators = document.getElementById("osc-generators")
 const audioContext = new AudioContext()
 const oscillators = []
 const oscNodes = []
@@ -40,10 +45,21 @@ let playing = false
 function addOscillator(e) {
     const newOsc = document.createElement("div")
     const oscBank = document.getElementById("osc-bank")
+    const oscTitle = document.createElement("h4")
+    const gainSlider = document.createElement("input")
+    const gainLabel = document.createElement("label")
+
+    gainSlider.type = "range"
+    gainSlider.name = "gain"
+    gainLabel.textContent = "gain"
+    
     if(e.target.nodeName==="BUTTON") {
-        newOsc.textContent = e.target.id
+        oscTitle.textContent = e.target.id
+        newOsc.append(oscTitle)
+        newOsc.append(gainLabel)
+        newOsc.append(gainSlider)
         oscBank.append(newOsc)
-        oscillators.push({type: `${e.target.id}`, frequency: 220})
+        oscillators.push({type: `${e.target.id}`})
     }
 }
 
@@ -52,15 +68,15 @@ function startSound(e) {
     
     if(Object.keys(keyboard).includes(input) && !keyboard[input].down) {
         oscillators.forEach(osc => {
-            console.log(osc)
-            oscNode = new OscillatorNode(audioContext, {type: osc.type, frequency: keyboard[input].freq})
+            const gainNode = audioContext.createGain()
+            const oscNode = new OscillatorNode(audioContext, {type: osc.type, frequency: keyboard[input].freq})
             oscNodes.push(oscNode)
-            oscNode.connect(audioContext.destination)
+            oscNode.connect(gainNode)
+            gainNode.gain = 
+            gainNode.connect(audioContext.destination)
             oscNode.start()
         })
         keyboard[input].down = true
-        console.log(keyboard[input].down)
-        console.log(keyboard)
     }
 }
 
@@ -69,11 +85,9 @@ function stopSound(e) {
 
     if(Object.keys(keyboard).includes(input)) {
         oscNodes.forEach(oscNode => {
-            oscNode.stop()
+            if (oscNode.frequency.value == keyboard[input].freq) oscNode.stop()
         })
         keyboard[input].down = false
-        console.log((keyboard[input].down))
-        console.log(keyboard)
     }
 }
 
